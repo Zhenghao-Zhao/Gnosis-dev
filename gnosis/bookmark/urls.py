aa086528878f1b047db89dc3ea9e38fecc15bcad
u5952700 <u5952700@anu.edu.au>
2019-08-26 16:02:24 +1000

    merge bookmark

---
+++ gnosis/bookmark/urls.py
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+from gnosis.bookmark import views
+
+# for updating/creating a Endorsement
+urlpatterns = [
+    path('bookmark', views.bookmark, name='bookmarks'),
+=======
+
+# for updating/creating a Endorsement
+urlpatterns = [
+    path('bookmark', views.bookmark, name='bookmarks'),
+>>>>>>> 42d415e6501159e4a0555db8f5cb6e572bcc0ddf
+]
---
+++ gnosis/bookmark/views.py
@@ -0,0 +1,89 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from .models import Bookmark, BookmarkEntry
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+
+<<<<<<< HEAD
+# @login_required
+# def bookmark(request):
+#
+#     bookmark = Bookmark.objects.filter(owner=request.user)
+#
+#     if not bookmark:
+#         bookmark = Bookmark()
+#         bookmark.owner = request.user
+#         bookmark.save()
+#
+#     message = None
+#     print("Bookmarks view!")
+#     return render(
+#         request, "bookmark.html", {"bookmark": bookmark, "message": message}
+#     )
+=======
+>>>>>>> 42d415e6501159e4a0555db8f5cb6e572bcc0ddf
+
+
+@login_required
+def bookmark(request):
+<<<<<<< HEAD
+
+    bookmark = Bookmark.objects.filter(owner = request.user)
+
+    if not bookmark:
+        user_bookmark = Bookmark()
+        user_bookmark.owner = request.user
+        user_bookmark.save()
+    else:
+        user_bookmark = bookmark[0]
+
+    if user_bookmark.owner == request.user:
+        papers = user_bookmark.papers.order_by('-created_at')
+
+=======
+
+    bookmark = Bookmark.objects.filter(owner = request.user)
+
+    if not bookmark:
+        user_bookmark = Bookmark()
+        user_bookmark.owner = request.user
+        user_bookmark.save()
+    else:
+        user_bookmark = bookmark[0]
+
+    if user_bookmark.owner == request.user:
+        papers = user_bookmark.papers.order_by('-created_at')
+
+>>>>>>> 42d415e6501159e4a0555db8f5cb6e572bcc0ddf
+        return render(request, "bookmark.html", {"bookmark": user_bookmark,
+                                                            "papers": papers, })
+
+    return HttpResponseRedirect(reverse("bookmark"))
+
+
+
+
+@login_required
+def bookmark_entry_remove(request, pid):
+    """Delete view"""
+    print("WARNING: Deleting bookmark entry with pid {}".format(pid))
+
+    # check if the bookmark for the user exists, if not, create one
+    try:
+        bookmark = Bookmark.objects.filter(owner = request.user)[0]
+        print("  ==> bookmark found")
+    except:
+        bookmark = Bookmark()
+        bookmark.owner = request.user
+        bookmark.save()
+
+
+    try:
+        b_entry = bookmark.papers.filter(paper_id=pid)[0]
+        print("   ==> entry found")
+        b_entry.delete()
+    except:
+        print("   ==> No such entry.")
+
+    return HttpResponseRedirect(reverse("paper_detail", kwargs={"id": pid, }))
