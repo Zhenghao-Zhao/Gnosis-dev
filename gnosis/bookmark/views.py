5ff9a92f60f7db98ccbf3746be3ebf9a3bc9fec3
Yuchong <32530268+Yuchong7733@users.noreply.github.com>
2019-08-26 15:46:29 +1000

    Update views.py
    remove unused methods

--- gnosis/bookmark/views.py
+++ gnosis/bookmark/views.py
@@ -5,16 +5,6 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 
 
-@login_required
-def bookmark(request):
-
-    bookmark = Bookmark.objects.filter(owner=request.user)
-
-    message = None
-    print("Bookmarks view!")
-    return render(
-        request, "bookmark.html", {"bookmark": bookmark, "message": message}
-    )
 
 
 @login_required
@@ -30,33 +20,6 @@ def bookmark_detail(request, id):
                                                             "papers": papers, })
 
     return HttpResponseRedirect(reverse("bookmark"))
-
-#
-# @login_required
-# def bookmark_entry_create(request, id, pid, title):
-#
-#     print("Adding paper to bookmark with pid: {}".format(pid))
-#
-#     # check if the bookmark for the user exists, if not, create one
-#     try:
-#         bookmark = get_object_or_404(Bookmark, pk=id)
-#     except:
-#         bookmark = Bookmark()
-#         bookmark.owner = request.user
-#
-#     # if this is POST request then add the entry
-#     if request.method == "POST" and bookmark.owner == request.user:
-#         bookmark_entry = BookmarkEntry()
-#         bookmark_entry.paper_id = pid
-#         bookmark_entry.paper_title = title
-#         bookmark_entry.bookmark = bookmark
-#
-#         try:
-#             BookmarkEntry.filter(bookmark=bookmark).filter(paper=pid)[0]
-#         except:
-#             bookmark_entry.save()
-#         return HttpResponseRedirect(reverse("paper_detail", kwargs={"id": pid}))
-#     return render(request, "paper_detail.html")
 
 
 @login_required
