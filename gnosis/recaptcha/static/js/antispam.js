7a143ef995e0a8358bebbef53323728dd51ec253
Zhenghao-Zhao <grekinaus@gmail.com>
2019-08-30 19:00:00 +1000

    minor fix

--- gnosis/recaptcha/static/js/antispam.js
+++ gnosis/recaptcha/static/js/antispam.js
@@ -24,16 +24,16 @@ function setButton(id) {
 /* callback functions for when different errors happen at client side verification */
 // when receive data (meaning client side verification is successful), allow submit button to work properly.
 function dataCallback() {
-    $('.captcha-submit').removeAttr('disabled');
+    $('.captcha-submit').prop('disabled', false);
 }
 
 // the response token has 2 mins before it expires, when that happens, disable the submit button (requires to re-verify).
 function dataExpiredCallback() {
-    $('.captcha-submit').attr('disabled', true);
+    $('.captcha-submit').prop('disabled', true);
 }
 
 // if received connection error, use a pop up window to remind user to re-verify, also disables the submit button.
 function dataErrorCallback() {
-    $('.captcha-submit').attr('disabled', true);
+    $('.captcha-submit').prop('disabled', true);
     window.alert("Network connectivity error, please retry reCaptcha.")
 }
