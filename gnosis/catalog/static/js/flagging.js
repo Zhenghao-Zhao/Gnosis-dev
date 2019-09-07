449661a684a172c26b4bb4acf36b65c091b0e046
Zhenghao-Zhao <grekinaus@gmail.com>
2019-09-07 11:56:38 +1000

    add prevent multisubmission

--- gnosis/catalog/static/js/flagging.js
+++ gnosis/catalog/static/js/flagging.js
@@ -12,19 +12,42 @@ function toggle_more(ele) {
     $(ele).siblings('.popup').attr('hidden', !hidden);
 }
 
-function open_dialog(id) {
-    $('.popup').attr('hidden', true);
-    $('.cover').attr('hidden', false);
+function open_dialog(id, authenticated) {
+    if (authenticated) {
+        $('.popup').attr('hidden', true);
+        $('.cover').attr('hidden', false);
 
-    // add comment id to te form
-    $("<input />").attr("type", "hidden")
-          .attr("name", "comment_id")
-          .attr("value", id)
-          .appendTo("#flag_form");
-    console.log(id)
+        // add comment id to te form
+        $("<input />").attr("type", "hidden")
+            .attr("name", "comment_id")
+            .attr("value", id)
+            .appendTo("#flag_form");
+    }else{
+
+    }
 }
 
-function cancel_form(){
+function cancel_form() {
     $('#flag_form').trigger('reset');
     $('.popup').attr('hidden', true);
 }
+
+// jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function () {
+    $(this).on('submit', function (e) {
+        var $form = $(this);
+
+        if ($form.data('submitted')) {
+            // Previously submitted - don't submit again
+            e.preventDefault();
+        } else {
+            // Mark it so that the next submit can be ignored
+            $form.data('submitted', true);
+        }
+    });
+
+    // Keep chainability
+    return this;
+};
+
+$('form').preventDoubleSubmission();
