326c0a4c11e5f07da50db5625294b57eff235439
Zhenghao-Zhao <grekinaus@gmail.com>
2019-10-08 17:46:10 +1100

    add more tests

--- gnosis/tests/test_view_flaggedcomment.py
+++ gnosis/tests/test_view_flaggedcomment.py
@@ -8,16 +8,7 @@ from django.http import HttpRequest
 
 
 class FlaggedCommentViewTest(TestCase):
-    def setUp(self):
-        # Create a user
-        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
-        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
-        self.user = User.objects.create_user(
-            username='gnosis', email='gnosis@gnosis.com', password='gnosis')
-
-        test_user1.save()
-        test_user2.save()
-
+    def setUpTestData(cls):
         # Create a paper at server
         paper = Paper()
         paper.title = "No title"
@@ -27,6 +18,17 @@ class FlaggedCommentViewTest(TestCase):
         paper.id = "01"
         paper.save()
 
+    def setUp(self):
+        # Create two users
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+        self.user = User.objects.create_user(
+            username='gnosis', email='gnosis@gnosis.com', password='gnosis')
+
+        test_user1.save()
+        test_user2.save()
+
+    # def test_
 
     # def test_fl_form
 
